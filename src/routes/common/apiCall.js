8db1c82e9613a2a4951201e81d5695c06d71fd3b
Nikunj Mochi <mochinikunj2@gmail.com>
2022-06-11 13:16:13 +0530

    adding GET or POST log for api req. method

--- src/routes/common/apiCall.js
+++ src/routes/common/apiCall.js
@@ -1,11 +1,21 @@
 const axios = require('axios');
 
 const getApiCall = () => {
+    console.log('Api Call:', { method: 'GET', endpoint, req });
 
+    return new Promise((resolve, reject) => {
+        axios.post(endpoint, req).then((res) => {
+            console.log('Success api call');
+            resolve(res);
+        }).catch((err) => {
+            console.log('Error in api call');
+            reject(err);
+        });
+    });
 };
 
 const postApiCall = (endpoint, req) => {
-    console.log('Api Call:', { endpoint, req });
+    console.log('Api Call:', { method: 'POST', endpoint, req });
 
     return new Promise((resolve, reject) => {
         axios.post(endpoint, req).then((res) => {
