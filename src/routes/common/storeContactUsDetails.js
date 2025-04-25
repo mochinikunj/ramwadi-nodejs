be8ec4354886cac6abc678530a00a0d2fd44eda3
Nidhi Patadia <nidhi.patadia@merkle.com>
2025-04-25 18:51:34 +0530

    Added drupal api call.

--- src/routes/common/storeContactUsDetails.js
+++ src/routes/common/storeContactUsDetails.js
@@ -1,16 +1,35 @@
+
+const config = require('config');
+const { postApiCall } = require('./apiCall');
+
 const storeContactUsDetails = async (body) => {
   console.log('storeContactUsDetails');
+  const endpoint = config.get('drupal.apiUrl');
 
   /**
    * Write a logic to make a call to drupal to store contact us details
    */
-
-  // default response
-  return {
-    status: 'OK',
-    code: '200',
-    success: true,
-  };
+  try {
+    const response = await postApiCall(endpoint, body);
+    if (!(response && response.data)) {
+      throw new Error();
+    }
+    return {
+      status: 'OK',
+      code: 200,
+      success: response.data.success,
+    };
+  } catch (err) {
+    console.log('Error in drupal response');
+    return {
+      status: 'NOK',
+      code: 400,
+      error: {
+        message: 'Error in drupal response',
+        error: err,
+      },
+    };
+  }
 };
 
 module.exports = storeContactUsDetails;
